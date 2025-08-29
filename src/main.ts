70a71c34ddcb6dfe6e7f82fed32dfee2faa4473f
邵艳福 <itheima@itcast.cn>
2025-08-29 16:22:26 +0800

    创建路由和页面布局基本组件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { createPinia } from 'pinia';
+import router from './router';
 
-createApp(App).mount('#app');
+//引入初始化样式文件
+import '@/styles/element/common.scss'
+
+const pinia = createPinia();
+const app = createApp(App);
+
+// 在一个应用实例上安装所有插件
+app.use(pinia);
+app.use(router);
+app.mount('#app');
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+</template>
